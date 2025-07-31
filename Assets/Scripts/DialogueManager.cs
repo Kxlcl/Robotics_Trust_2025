f2165cb801bd76c6583752f0b26f640a787a8816
Kelly Lin <linkelly150@gmail.com>
2025-07-31 17:22:00 -0400

    Basic Scene setup

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogueLines;
+    public float textSpeed = 0.05f;
+
+    private int currentLine = 0;
+    private Coroutine typingCoroutine;
+
+    public void StartDialogue()
+    {
+        dialoguePanel.SetActive(true);
+        currentLine = 0;
+        ShowLine();
+    }
+
+    public void NextLine()
+    {
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            dialogueText.text = dialogueLines[currentLine];
+            typingCoroutine = null;
+            return;
+        }
+        currentLine++;
+        if (currentLine < dialogueLines.Length)
+        {
+            ShowLine();
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    void ShowLine()
+    {
+        typingCoroutine = StartCoroutine(TypeLine(dialogueLines[currentLine]));
+    }
+
+    IEnumerator TypeLine(string line)
+    {
+        dialogueText.text = "";
+        foreach (char c in line)
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+        typingCoroutine = null;
+    }
+
+    void EndDialogue()
+    {
+        dialoguePanel.SetActive(false);
+    }
+}
